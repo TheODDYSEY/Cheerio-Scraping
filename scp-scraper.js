2c4966347ad4205af3f26b0d50e68253868a3a3e
TheODDYSEY <mosessabz07@gmail.com>
2024-02-08 23:45:22 +0300

    Added docs 📃 for function part 1

--- scp-scraper.js
+++ scp-scraper.js
@@ -8,22 +8,33 @@ const url = "https://scp-wiki.wikidot.com/scp-series";
 
 async function scrapeData() {
   try {
+    // use axios to get the HTML from the page
     const { data } = await axios.get(url);
+    // load the HTML into cheerio
     const $ = cheerio.load(data);
 
+    // select the list items within the element with class selector "content-panel"
     const listItems = $(".content-panel ul li"); // Corrected selector to target the list items containing SCP entries
 
+    // array o hold the scraped data
     const creatures = [];
 
+    // iterate over each list item
     listItems.each((idx, el) => {
+        // create an object to hold the data for each creature
       const creature = { id: "", name: "" };
 
+    //   get the creatures ID from the text of the child anchor tag
       creature.id = $(el).children("a").attr("href"); // Corrected to get the href attribute of the anchor tag
+
+    //   get the creatures name from the text content of the list item
       creature.name = $(el).text().trim(); // Just getting the text content of the list item
 
+    //   add the creature to the array
       creatures.push(creature);
     });
 
+    // log the array to the console
     console.log(creatures);
 
     fs.writeFileSync(
